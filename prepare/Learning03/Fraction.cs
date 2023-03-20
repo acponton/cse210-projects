531579847c48180a2f48169b262e38fb14deb30c
acponton <pon22008@byui.edu>
2023-03-19 19:04:56 -0500

    Learning Activity developed

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,61 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    // Create the following constructors:
+    // * Constructor that has no parameters that initializes the number to 1/1.
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // * Constructor that has one parameter for the top and that initializes the denominator to 1. 
+    // So that if you pass in the number 5, the fraction would be initialized to 5/1.
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    // * Constructor that has two parameters, one for the top and one for the bottom.
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string newFraction = $"{_top}/{_bottom}";
+        return newFraction; 
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top/(double)_bottom;
+    }
+}
